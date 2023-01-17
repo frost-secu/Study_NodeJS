4bde91378f7dd2294a47079cc33b17e49a277bc0
frost-secu <shw9810@icloud.com>
2023-01-17 21:57:22 +0900

    day1

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+//  Node JS < Back-end area >
 var express = require('express')
 var app = express()
 
+app.use(express.static('Public'))
+
 app.get('/', function(req, res) {
 	// res.send('<h1>Hello World</h1>')
-	res.sendFile(__dirname + "/Public/index.html")
+	res.sendFile(__dirname + "/index.html")
 });
 
 app.listen(8080, function() {
